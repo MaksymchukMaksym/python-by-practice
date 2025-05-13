ca2373445cbdef93445aa18f91d10d1dbd9a6e09
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2025-05-14 01:04:51 +0300

    create architecture for testing all modules(main.py)

---
+++ core-python/main.py
@@ -0,0 +1,40 @@
+import runpy
+import os
+
+#список модулів
+MODULES =[
+    "loops",
+    "variables_and_types",
+    "operators",
+    "if_else"
+]
+
+def show_modules():
+    print("Available modules")
+    for i,names in enumerate(MODULES):
+        print(f"modules {i+1}: {names}")
+    print("q. Quit")
+
+def run_modules(module_name):
+    try:
+        runpy.run_path(f"{module_name}.py", run_name="__main__")
+    except Exception as e:
+        print(f"Failed to run module '{module_name}': {e}")
+
+def main():
+    while True:
+        show_modules()
+        choice = input("Choose module for test by number: ")
+        if choice == 'q':
+            break
+        try:
+            index = int(choice)-1
+            if 0<=index<len(MODULES):
+                run_modules(MODULES[index])
+            else:
+                print("Invalid number")
+        except ValueError:
+            print("Enter a number")
+
+if __name__=="__main__":
+    main()
