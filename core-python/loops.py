567ffd2a60f64aae230905a06909aee7383cb754
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2025-05-13 13:47:48 +0300

    fix tasks 5,6,8 loops.py

--- core-python/loops.py
+++ core-python/loops.py
@@ -27,44 +27,41 @@ def task_3(a,b):
 #Вивести таблицю множення (від 1 до 10).
 def task_4():
     for number in range(1,10):
-        for multiplier in range(1,10):
+        for multiplier in range(1,11):
             print(f"{number}: {number} * {multiplier} = {number*multiplier}")
 
 #Вивести перші n чисел послідовності Фібоначчі.
-def task_5():
-    if n<=0:
-        return 0
-    if n==1:
-        return 1
-    a,b=0
-    for _ in range(2,n+1):
-        a,b=b,a+b
-    return b
+def task_5(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
 
 #Зчитувати числа, поки користувач не введе 0. Потім вивести їхню суму.
 def task_6():
     sum=0
     while True:
-        number = input("Enter number, zero for stop: ")
+        try:
+            number = int(input("Enter number, zero for stop: "))
+        except:
+            print("Invalid input.")
+            continue
         if number == 0:
             break
-        try:
-            sum+=number
-        except:
-            print("Invalid number")
+        sum+=number
 
-    return number
+    print("Sum: ",sum)
 
 #Намалювати прямокутник з зірочок розміром NxM (вкладений цикл).
 def task_7(n, m):
     for _ in range(n):
         for _ in range(m):
-            print("*")
+            print("*", end="")
 
 #Знайти перше число від 1 до 1000, яке ділиться на 17, 13 і 19 одночасно.
 def task_8():
     for number in range(1, 1000):
-        if number%17 == 0 and number%13 == 0 and number%18 == 0:
+        if number%17 == 0 and number%13 == 0 and number%19 == 0:
             return number
 
 #Вивести всі прості числа до 100.
