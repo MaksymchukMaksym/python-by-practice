030b44842160993a83d21489a7c4c57e2b9b8890
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2025-05-14 12:30:06 +0300

    correcting testing architecture()

--- core-python/main.py
+++ core-python/main.py
@@ -1,25 +1,27 @@
 import runpy
-import os
 
-#список модулів
-MODULES =[
+# список модулів
+MODULES = [
     "loops",
     "variables_and_types",
     "operators",
     "if_else"
 ]
 
+
 def show_modules():
     print("Available modules")
-    for i,names in enumerate(MODULES):
-        print(f"modules {i+1}: {names}")
+    for i, names in enumerate(MODULES):
+        print(f"modules {i + 1}: {names}")
     print("q. Quit")
+
 
 def run_modules(module_name):
     try:
         runpy.run_path(f"{module_name}.py", run_name="__main__")
     except Exception as e:
         print(f"Failed to run module '{module_name}': {e}")
+
 
 def main():
     while True:
@@ -28,13 +30,14 @@ def main():
         if choice == 'q':
             break
         try:
-            index = int(choice)-1
-            if 0<=index<len(MODULES):
+            index = int(choice) - 1
+            if 0 <= index < len(MODULES):
                 run_modules(MODULES[index])
             else:
                 print("Invalid number")
         except ValueError:
             print("Enter a number")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
